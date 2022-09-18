408eb6290dc461baac0d63ecd0af6c5122510ba3
yesi fita <yesifita@gmail.com>
2022-09-18 12:58:52 -0300

    agrego colores predeterminados de paleta y cambios en estilo boton diseño responsivo

--- src/componentes/boton/Boton.js
+++ src/componentes/boton/Boton.js
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Boton = () => {
 return (
-    <button type="button" className="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 mt-2 mb-3 mx-3 pt-3 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 float-right 
+    <>
+    <button type="button" className="text-white bg-fondo-boton hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-semibold tracking-wider rounded-lg text-md px-5 py-2.5 mx-3 mb-4 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700 float-right lg:py-2 lg:mx-3 lg:mt-0 lg:mb-1 lg:text-lg lg:font-medium
     ">Reservar</button>
+
+{/* fonfo original de boton de tailwind bg-gray-800 */}
+    </>
 )
 }
 
--- src/componentes/cards/Cards.js
+++ src/componentes/cards/Cards.js
@@ -9,10 +9,11 @@ const Cards = ({ item }) => {
     <figure
       class="card-conteiner"
       key={item.id}
-      className=" max-w-130 h-150 border-solid border-4 border-amber-700 rounded-md gap-y-1 mx-3 mb-3 bg-slate-600 text-white flex-column md:w-5/6 md:h-full md:gap-1 "
-    >
+      className=" max-w-130 h-150 border-solid border-4 border-naranja rounded-md gap-y-1 mx-3 mb-3 bg-gris-fondo2 text-white flex-column md:w-5/6 md:h-full md:gap-1  "
+    >     {/* --- fondo original de tailwind para contenedor de card bg-slate-700 */}
+
       <div class="card-body" className="h-130 p-1 m-2 md:max-h-100">
-        <h3 className="text-lg font-bold mb-2 p-2 text-center capitalize tracking-wide md:text-xl  md:font-semibold lg:font-bold md:tracking-wider md:h-6 md:mb-4 md:mt-0 md:pt-1">
+        <h3 className="text-2xl font-bold mb-2 p-2 text-center capitalize tracking-wider md:text-xl  md:font-semibold  md:tracking-wider md:h-6 md:mb-4 md:mt-0 md:pt-1">
           {item.nombre}
         </h3>
 
@@ -24,7 +25,7 @@ const Cards = ({ item }) => {
       </div>
       <figcaption class="card-footer" className=" h-36 md:h-44 md:mb-0 md:pb-1">
         <div className="md:h-44 md:mb-4 lg:mb-4">
-          <p className="font-medium text-lg text-justify px-4 pt-1 mt-2 mb-3  break-normal  md:mb-1 md:font-semibold md:text-lg">
+          <p className="font-medium text-xl text-justify px-4 pt-1 mt-2 mb-3  break-normal  md:mb-1 md:font-semibold md:text-lg md:tracking-tight">
             {item.textoCorto}
           </p>
         </div>
@@ -38,8 +39,10 @@ const Cards = ({ item }) => {
         </div>
         {visible ? (
           <div class="modal-cajon-container " className="bg-zinc-200 fixed inset-0 z-50 opacity-100 ">
-              <div class="modal-container" className="w-screen h-auto  bg-zinc-200  flex flex-col  pt-2 p-5 border-4 border-orange-700 rounded-xl ">
-                <div class="modal" className=" max-h-500 max-w-600 flex flex-col text-lg text-zinc-600 mb-10">
+              <div class="modal-container" className="w-screen h-auto bg-gris-claro-palete  flex flex-col  pt-2 p-5 border-4 border-orange-700 rounded-xl ">
+              {/* fondo original de tailwind para el modal bg-zinc-200 */}
+
+                <div class="modal" className="w-auto h-auto flex flex-col text-lg text-zinc-600 mb-10">
                   <button onClick={() => setVisible(false)}>
                     <svg
                       className="float-right h-8 p-0 m-0"
@@ -51,15 +54,15 @@ const Cards = ({ item }) => {
                     </svg>
                   </button>
 
-                  <div className="w-auto h-full p-0 pb-4 m-0 mb-2 text-center font-bold border-b-2 border-stone-500 md:text-3xl md:font-semibold ">
+                  <div className="w-auto h-full p-0 pb-4 m-0 mb-2 capitalize text-center font-bold border-b-2 border-stone-500 md:text-3xl  md:tracking-wider">
                     {item.nombre}
                   </div>
 
-                  <div className="w-auto h-auto px-1 font-medium text-center text-stone-500 indent-0  leading-6 md:leading-8 md:font-semibold md:text-2xl">
+                  <div className="w-auto h-auto px-1 font-medium text-justify text-stone-500 indent-0 m-2 mx-3 p-1 leading-6 md:leading-8 md:font-semibold md:text-2xl md:tracking-wide md:pb-2 md:mb-1">
                     {item.texto}
                   </div>
 
-                  <div className="">
+                  <div className="md:mx-0 px-0">
                     <Boton />
                   </div>
                 </div>
