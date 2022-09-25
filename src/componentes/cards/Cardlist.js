a86339cf070ed515af6bc22552ac23f8b2625770
yesicafita <yesifita@gmail.com>
2022-09-25 17:45:11 -0300

    coloco estilos en variables

--- src/componentes/cards/Cardlist.js
+++ src/componentes/cards/Cardlist.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import Cards from './Cards';
+import *as  styles from './styles';
 
 const Cardlist = ({data}) => {
   return (
     <>
-      <div class="container" className="w-screen h-full flex flex-row flex-wrap justify-content  bg-neutral-300 overflow-hidden md:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-2 md:px-4 md:py-3 overflow-y-hidden">
+      <div className={styles.CARDLIST_CONT}>
         {data.map((item) => (
           <Cards
             key={item.id}
