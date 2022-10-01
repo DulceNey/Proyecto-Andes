0e259dcbcd45d687287ae2840246391b60c658f4
yesicafita <yesifita@gmail.com>
2022-10-01 00:33:33 -0300

    arreglar drawer anclado a etiqueta a

--- src/componentes/Cajonbarra.js
+++ src/componentes/Cajonbarra.js
@@ -1,38 +1,42 @@
 import React from "react";
 import *as estilos from './estilos'
 
-const Cajonbarra = () => {
+
+
+const Cajonbarra = ({setVisible}) => {
+
+ 
   return (
     <>
       <nav
        
         className={estilos.NAV}
       >
-        <a
+        <a onClick={() => setVisible((visible) => !visible)}
           href="#"
-          value="a"
           className={estilos.A}
-        >
-          <h2 className="m-0">Inicio</h2>
+          >
+
+          <h2 className="m-0" >Inicio</h2>
         </a>
         <a
           href="#excursiones"
-          value="a"
           className={estilos.A}
+          onClick={() => setVisible((visible) => !visible)}
         >
           <h2 className="m-0">Excursiones</h2>
         </a>
         <a
           href="#conocenos"
-          value="a"
           className={estilos.A}
+          onClick={() => setVisible((visible) => !visible)}
         >
           <h2 className="m-0">Conocenos</h2>
         </a>
         <a
           href="#contacto"
-          value="a"
           className={estilos.A}
+          onClick={() => setVisible((visible) => !visible)}
         >
           <h2 className="m-0">Contacto</h2>
         </a>
