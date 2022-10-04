7ceca6bccc0d941041fc5715911ec48b41e13e7a
Ney <terapiaintegralney@hotmail.com>
2022-10-04 14:06:40 -0300

    Correccion en colores

--- src/Components/Boton.js
+++ src/Components/Boton.js
@@ -1,6 +1,6 @@
 const Boton = () => {
     return (
-        <button className="bg-amber-700	 text-neutral-300	 w-32	h-10	h p-0	 rounded-md	text-center my-5"
+        <button className="bg-orange-600	 text-neutral-300	 w-32	h-10	h p-0	 rounded-md	text-center my-5"
         >Descubre más</button>
     )
 }
--- src/Components/Componentes-Completos/Presentacion.js
+++ src/Components/Componentes-Completos/Presentacion.js
@@ -4,7 +4,7 @@ import Lista from "../Lista"
 
 export const Presentacion = () => {
 return (
-    <div className="bg-orange-500 border-amber-900 border-4" >
+    <div className="bg-amber-800 border-orange-600 border-4" >
 <Imagen1/>
 <h1 className="text-center text-4xl text-neutral-300" > ¿Que hace unico a Los Andes: Aventura y Turismo?</h1>
 <p className="text-center my-2.5 text-2xl text-neutral-300"> Nuestro deseo de reavivar la pasión por el turismo, el explorar y divertirse junto a otros campistas y/o amantes de la naturaleza, la aventura y los lazos.</p>
