83b7cb0310ebea0a57f13bfd30b399becdb49791
Ney <terapiaintegralney@hotmail.com>
2022-10-24 22:21:41 -0300

    Componentes de Ney anexados con exito

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
 import React from "react";
-import Cardlist from "./componentes/cards/Cardlist";
-//import ShoppingCart from "../src/componentes/ShoppingCart";
-import ToursProvider from "./componentes/ToursProvider";
-import Navbar from "./componentes/Navbar";
-
 import Carruselcompleto from "./Components/Componentes-Completos/Carruselcompleto";
+import Oferta from "./Components/Componentes-Completos/Oferta";
+import Presentacion from "./Components/Componentes-Completos/Presentacion"
 function App() {
   
   return (
     <>
-  
-    <ToursProvider>
-      <Navbar/>
-      <Cardlist  /> 
-      {/* <ShoppingCart/>  */}
-
-    </ToursProvider>
-
-
-
-        
-    </>
-  
+<Presentacion/>
+<Oferta/>
+<Carruselcompleto/>
+</>
   );
 }
 
