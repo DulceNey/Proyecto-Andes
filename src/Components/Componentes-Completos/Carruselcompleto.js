37071eba9c2ee090788535f2d28b0557d2660145
Ney <terapiaintegralney@hotmail.com>
2022-10-24 21:34:49 -0300

    Correccion de colores en modo claro

--- src/Components/Componentes-Completos/Carruselcompleto.js
+++ src/Components/Componentes-Completos/Carruselcompleto.js
@@ -3,9 +3,9 @@ import Carrusel from "../Carrusel";
 
 const Carruselcompleto = () => {
     return (
-        <div className="bg-slate-500 w-full">
+        <div className="bg-slate-300 w-full">
 
-<h1 className="text-neutral-300 text-4xl mx-7 text-center mb-1.5">Disfrute de bellas fotografías de nuestras zonas más destacadas</h1>
+<h1 className="text-black text-4xl mx-7 text-center mb-1.5">Disfrute de bellas fotografías de nuestras zonas más destacadas</h1>
 <Carrusel/>
         </div>
     );
