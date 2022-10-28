1a74769fa640d445e768ecf468d14934a7dc6b0c
sarah <saritahszwajko@gmail.com>
2022-10-28 01:10:55 -0300

    mas cambios

--- src/Components/Foteer.js
+++ src/Components/Foteer.js
@@ -9,7 +9,7 @@ const Foteer = () => {
     <div className="sm:flex sm:items-center sm:justify-between">
         <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0">
         
-            <span class="self-center text-2xl not-italic text-white whitespace-nowrap dark:text-white">Andes y Turismo</span>
+            <span class="self-center text-2xl not-italic text-white whitespace-nowrap dark:text-white">Andes Aventura y Turismo</span>
         </a>
         <ul className="flex flex-wrap items-center mb-6 text-sm not-italic text-white sm:mb-0 dark:text-gray-400">
             <li>
@@ -27,12 +27,12 @@ const Foteer = () => {
         </ul>
     </div>
     < a href= "#" className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8"></a>
-    <span className="block text-sm text-white sm:text-center dark:text-white">© 2022 <a href="https://flowbite.com/" className="hover:underline">Andes y Turismo Group™</a>.Todos los Derechos Reservados.
+    <span className="block text-sm text-white sm:text-center dark:text-white">© 2022 <a href="https://flowbite.com/" className="hover:underline">Andes Aventura y Turismo Group™</a>.Todos los Derechos Reservados.
     </span>
 </h1>
     </div>
     </>
   )
-}
+  }
 
 export default Foteer
