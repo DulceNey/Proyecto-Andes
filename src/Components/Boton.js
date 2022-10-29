11569067962ce8eddab7e54eed72c85fa97de770
Ney <terapiaintegralney@hotmail.com>
2022-10-28 21:17:52 -0300

    Modo oscuro para boton, e imagenes

--- src/Components/Boton.js
+++ src/Components/Boton.js
@@ -1,6 +1,6 @@
 const Boton = () => {
   return (
-    <button className="bg-amber-700	 text-neutral-300	 w-32	h-10	h p-0	 rounded-md	text-center my-5">
+    <button className="bg-amber-700	 text-black	 w-32	h-10	h p-0	 rounded-md	text-center my-5 dark:bg-gray-700">
       Descubra más
     </button>
   );
